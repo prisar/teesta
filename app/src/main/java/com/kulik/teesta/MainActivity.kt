38ea7c863880d3631bedf6f55cf33fbaf32e1f01
prisar <prisar230@gmail.com>
2023-01-30 11:06:05 +0530

    image

--- app/src/main/java/com/kulik/teesta/MainActivity.kt
+++ app/src/main/java/com/kulik/teesta/MainActivity.kt
@@ -3,16 +3,17 @@ package com.kulik.teesta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kulik.teesta.ui.theme.TeestaTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,6 +38,14 @@ fun Home() {
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween) {
         Text("Teesta app for north bengal")
+
+        Image(
+            painter = painterResource(id = R.drawable.dalimgaon),
+            contentDescription = "train",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
 
         Text("Wiki for outsiders")
     }
