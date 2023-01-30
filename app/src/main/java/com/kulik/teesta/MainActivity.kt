bb402ca9d25cf1fcbf86839c00a6e7ac9e220a85
prisar <prisar230@gmail.com>
2023-01-30 10:56:13 +0530

    text

--- app/src/main/java/com/kulik/teesta/MainActivity.kt
+++ app/src/main/java/com/kulik/teesta/MainActivity.kt
@@ -3,11 +3,14 @@ package com.kulik.teesta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kulik.teesta.ui.theme.TeestaTheme
@@ -31,7 +34,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween) {
+        Text("Teesta app for north bengal")
+
+        Text("Wiki for outsiders")
+    }
 }
 
 @Preview(showBackground = true)
