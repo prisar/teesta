11d3b90172e60017d211af8190f007a548d5ca8b
prisar <prisar230@gmail.com>
2023-05-13 00:48:18 +0530

    ide upgrade

--- app/src/main/java/com/kulik/teesta/ui/theme/Theme.kt
+++ app/src/main/java/com/kulik/teesta/ui/theme/Theme.kt
@@ -30,7 +30,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun TeestaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette // DarkColorPalette
     } else {
         LightColorPalette
     }
